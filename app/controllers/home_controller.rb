db26027f66977af97e4aa2ef8ac4a3617324ff4b
ro-mo <1matteodeluca@gmail.com>
2024-01-18 23:03:43 +0100

    search bar and select implemented

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,14 @@
 class HomeController < ApplicationController
   skip_before_action :authenticate, only: %i[ index ]
   def index
+    @categories = Room.distinct.pluck(:category)
     @rooms = Room.all
+
+    if params[:q].present?
+      @rooms = @rooms.where('name LIKE ?', "%#{params[:q]}%")
+    end
+    if params[:category].present?
+      @rooms = @rooms.where(category: params[:category])
+    end
   end
 end
